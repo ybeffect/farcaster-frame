f16959ed5718eb8b5016f696b770aab669f479db
ybeffect <yashbora95@gmail.com>
2024-01-28 11:47:13 -0500

    changed front page to million logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,15 +5,31 @@ export default function Home() {
   return (
     <>
       <Head>
-        <meta property="og:title" content="Frame" />
-        <meta property="og:image" content={`${BASE_URL}/question.jpg`} />
+        <meta property="og:title" content="Who Wants to Be a Millionaire - Play Now!" />
+        <meta property="og:image" content={`${BASE_URL}/millionlogo.jpeg`} />
         <meta property="fc:frame" content="vNext" />
-        <meta property="fc:frame:image" content={`${BASE_URL}/question.jpg`} />
-        <meta property="fc:frame:button:1" content="Yes" />
-        <meta property="fc:frame:button:2" content="No" />
+        <meta property="fc:frame:image" content={`${BASE_URL}/millionlogo.png`} />
+        <meta property="fc:frame:button:1" content="Play Now!" />
         <meta property="fc:frame:post_url" content={`${BASE_URL}/api/post`} />
       </Head>
     </>
   )
 }
-//commit to deploy
+// commit to deploy
+
+
+// export default function Home() {
+//   return (
+//     <>
+//       <Head>
+//         <meta property="og:title" content="Frame" />
+//         <meta property="og:image" content={`${BASE_URL}/question.jpg`} />
+//         <meta property="fc:frame" content="vNext" />
+//         <meta property="fc:frame:image" content={`${BASE_URL}/question.jpg`} />
+//         <meta property="fc:frame:button:1" content="Yes" />
+//         <meta property="fc:frame:button:2" content="No" />
+//         <meta property="fc:frame:post_url" content={`${BASE_URL}/api/post`} />
+//       </Head>
+//     </>
+//   )
+// }
